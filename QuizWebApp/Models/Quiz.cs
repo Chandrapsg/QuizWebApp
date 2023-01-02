046b162a22808f1dd6686875711d517c8974e6b4
Chandrapsg <89073610+Chandrapsg@users.noreply.github.com>
2023-01-02 14:03:34 +0530

    Add project files.

---
+++ QuizWebApp/Models/Quiz.cs
@@ -0,0 +1,15 @@
+﻿namespace QuizWebApp.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public String QuizQuestion { get; set; }
+        public String QuizAnswer { get; set; }
+
+        public Quiz()
+        {
+
+        }
+
+    }
+}
